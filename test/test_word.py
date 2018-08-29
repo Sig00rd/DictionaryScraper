dd60805ea83751bf1b539e953f11a9149e3ac275
Sig00rd <hamletolog@gmail.com>
2018-08-29 16:03:08 +0200

    Fix wrong module names

--- test/test_word.py
+++ test/test_word.py
@@ -6,12 +6,12 @@ from dictscraper.word import Word
 class WordTestCase(unittest.TestCase):
 
     def test_csv_template(self):
-        w1 = Word("foo", "bar", "foo, bar")
-        self.assertEqual("foo; bar; foo, bar", w1.csv())
+        w1 = Word("foo", "bar", "foo,bar")
+        self.assertEqual("foo; bar; foo,bar", w1.csv())
 
     def test_word_constructor_fields(self):
-        w1 = Word("foo", "bar", "foo, bar")
-        self.assertEqual(w1.fields, ["foo", "bar", "foo, bar"])
+        w1 = Word("foo", "bar", "foo,bar")
+        self.assertEqual(w1.fields, ["foo", "bar", "foo,bar"])
 
 
 if __name__ == "__main__":
