ba0ce8a9e0f95250be81e897578a7a394dff4f1d
Sig00rd <hamletolog@gmail.com>
2018-09-12 21:05:57 +0200

    Refactor file handler

--- dictscraper/file_handler.py
+++ dictscraper/file_handler.py
@@ -7,20 +7,9 @@ class FileHandler:
         home_directory_path = os.path.expanduser("~")
         self.file_path = os.path.join(home_directory_path, config.CSV_FILE_PATH)
         self.file = open(self.file_path, "a+")
-        self.file_empty = self.is_file_empty()
 
     def __del__(self):
         self.file.close()
 
-    def is_file_empty(self):
-        if os.path.getsize(self.file_path) > 0:
-            return False
-        return True
-
     def append_to_file(self, string_to_append):
-        if self.file_empty:
-            self.file.write(string_to_append)
-            self.file_empty = False
-
-        else:
-            self.file.write("\n" + string_to_append)
+        self.file.write(string_to_append + "\n")
